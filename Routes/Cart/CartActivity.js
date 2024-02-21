c2eab5585b7c01fc74d96685505daeb3184d5c9e
AmanKrs <imkumarak4@gmail.com>
2024-02-21 20:55:38 +0530

    add to cart function created

--- Routes/Cart/CartActivity.js
+++ Routes/Cart/CartActivity.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
 const { Users, Cart, Session } = require("../../Schema/CustomerSchema");
 const RestaurantInfo = require("../../Schema/RestaurantSchema");
 const RestaurantsMenu = require("../../Schema/RestaurantMenuSchema");
@@ -9,16 +8,58 @@ const RestaurantsMenu = require("../../Schema/RestaurantMenuSchema");
 router.post("/addtocart", async (req, res) => {
   const isvalid = jwt.verify(req.headers.authorization, "secret");
   if (isvalid) {
-    console.log(req.body.addItem);
+    // console.log(req.body.addItem);
     const itemList = await RestaurantsMenu.find({ _id: req.body.item._id });
-    const qauntity = req.body.addItem + 1;
-    const cartItem = {
-      usrid: isvalid.uid,
-      itemid: req.body.item._id,
-      qauntity: qauntity,
-    };
+    // console.log(itemList);
+    const quantity = req.body.addItem + 1;
 
-    console.log(cartItem);
+    const cart_for_user = await Cart.findOne({
+      user_id: isvalid.uid,
+      item_id: req.body.item._id,
+    });
+    console.log("check cart exist", cart_for_user);
+    if (cart_for_user) {
+      const updateCartItem = await Cart.findOneAndUpdate(
+        { user_id: isvalid.uid, item_id: req.body.item._id },
+        { quantity: quantity }
+      );
+    } else {
+      const cartItem = {
+        user_id: isvalid.uid,
+        item_id: req.body.item._id,
+        quantity: quantity,
+      };
+
+      const addnewItem = new Cart(cartItem);
+
+      const addItem_cart = await addnewItem.save();
+      if (addItem_cart) {
+        console.log(cartItem);
+      }
+    }
+  }
+});
+
+
+
+router.post("/removeCartItem", async (req, res) => {
+  const isvalid = jwt.verify(req.headers.authorization, "secret");
+  if (isvalid) {
+    const quantity = req.body.addItem - 1;
+    console.log("remove quantity", quantity);
+    const cart_for_user = await Cart.findOne({
+      user_id: isvalid.uid,
+      item_id: req.body.item._id,
+    });
+    // console.log("check cart exist", cart_for_user);
+    if (cart_for_user) {
+      const removeItem = await Cart.findOneAndUpdate(
+        { user_id: isvalid.uid, item_id: req.body.item._id },
+        { quantity: quantity }
+      );
+
+      console.log(removeItem);
+    }
   }
 });
 
